ba7b9459b1e37a736b49f2ef7a90837f3ba33697
mihaios1k <42968182+mihaios1k@users.noreply.github.com>
2020-10-18 23:40:50 +0500

    Add files via upload

---
+++ scp-079.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main(){
+	cout << "Exidy Inc. 1978" << endl;
+	Sleep(5000);
+	cout << "OS starts" << endl;
+	Sleep(10000);
+	cout << "All OK!" << endl;
+}
+
